c7842f0cd1860278a3bd8c20fd0cbe943a95d308
Loïs Boubault <7613286+Applelo@users.noreply.github.com>
2024-02-11 20:41:03 +0100

    working on composable

--- src/components/Blottie.vue
+++ src/components/Blottie.vue
@@ -1,76 +1,11 @@
 <script lang="ts" setup>
-import type {
-  AnimationEventName,
-  AnimationItem,
-  AnimationSegment,
-  CanvasRendererConfig,
-  HTMLRendererConfig,
-  LottiePlayer,
-  RendererType,
-  SVGRendererConfig,
-} from 'lottie-web'
+import type { AnimationEventName } from 'lottie-web'
 import { onMounted, onUnmounted, ref } from 'vue'
-import type { BlottieEmitEvents, BlottiePlayer } from '../typings/blottie'
-import getPlayer from './../utils/getPlayer'
+import type { BlottieEmitEvents, BlottieOptionsComponent } from '../typings/blottie'
+import { useBlottie } from './../composables/blottie'
 
 const props = withDefaults(
-  defineProps<{
-    /**
-     * Tag of the container
-     *
-     * @default 'div'
-     */
-    containerTag?: string
-    /**
-     * Lottie player loaded by Blottie
-     *
-     * @default 'undefined'
-     */
-    player?: BlottiePlayer
-    /**
-     * Callback before loadAnimation allowing to setLocationHref to fix Safari issue
-     *
-     * @see https://github.com/airbnb/lottie-web#issues
-     * @default 'undefined'
-     */
-    beforeInit?: (lottie: LottiePlayer) => Promise<void>
-
-    /**
-     * The relative path to the animation object
-     */
-    path?: string
-    /**
-     * An Object with the exported animation data.
-     */
-    animationData?: any
-    /**
-     * 'svg' / 'canvas' / 'html' to set the renderer
-     */
-    renderer?: RendererType
-    loop?: boolean | number
-    /**
-     * Start playing as soon as it is ready
-     */
-    autoplay?: boolean
-    initialSegment?: AnimationSegment
-    /**
-     * Animation name for future reference
-     */
-    name?: string
-    assetsPath?: string
-    rendererSettings?: {
-      svg: SVGRendererConfig
-      canvas: CanvasRendererConfig
-      html: HTMLRendererConfig
-    }[RendererType]
-    audioFactory?: (assetPath: string) => {
-      play: () => void
-      seek: () => void
-      playing: () => void
-      rate: () => void
-      setVolume: () => void
-    }
-  }>(),
+  defineProps<BlottieOptionsComponent>(),
   {
     containerTag: 'div',
   },
@@ -94,15 +29,13 @@ const events: AnimationEventName[] = [
 ]
 
 const container = ref<HTMLElement>()
-const lottie = ref<LottiePlayer>()
-const anim = ref<AnimationItem>()
+const { lottie, anim } = useBlottie(container, props as any)
 const pending = ref<boolean>(true)
 
 onMounted(async () => {
-  if (!container.value || typeof window === 'undefined')
+  if (!container.value || typeof window === 'undefined' || !lottie.value)
     return
 
-  lottie.value = await getPlayer(props.renderer, props.player)
   if (props.beforeInit)
     await props.beforeInit(lottie.value)
 
--- src/typings/blottie.ts
+++ src/typings/blottie.ts
@@ -1,4 +1,4 @@
-import type { AnimationEventName, AnimationItem, LottiePlayer } from 'lottie-web'
+import type { AnimationConfigWithData, AnimationConfigWithPath, AnimationItem, LottiePlayer, RendererType } from 'lottie-web'
 
 export type BlottiePlayer =
   | 'canvas_worker'
@@ -17,12 +17,6 @@ export interface BlottieExpose {
   container?: HTMLElement
 }
 
-export type BlottieEmitEvents = Record<AnimationEventName | 'ready', [
-  anim?: AnimationItem,
-  lottie?: LottiePlayer,
-  container?: HTMLElement,
-]>
-
 // export interface BlottieOptions {
 //   /**
 //    * Lottie player loaded by Blottie
@@ -38,3 +32,113 @@ export type BlottieEmitEvents = Record<AnimationEventName | 'ready', [
 //    */
 //   beforeInit?: (lottie: LottiePlayer) => Promise<void>
 // }
+
+// export type BlottieEmitEvents = Record<AnimationEventName | 'ready', [
+//   anim?: AnimationItem,
+//   lottie?: LottiePlayer,
+//   container?: HTMLElement,
+// ]>
+
+/* eslint-disable ts/consistent-type-definitions */
+export type BlottieEmitEvents = {
+  ready: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  enterFrame: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  loopComplete: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  complete: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  segmentStart: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  destroy: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  config_ready: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  data_ready: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  DOMLoaded: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  error: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  data_failed: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  loaded_images: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+  drawnFrame: [
+    anim?: AnimationItem,
+    lottie?: LottiePlayer,
+    container?: HTMLElement,
+  ]
+}
+
+type AnimationConfigWith<T extends RendererType = 'svg'> = AnimationConfigWithPath<RendererType> | AnimationConfigWithData<RendererType>
+
+type BlottieOptionsPart<T extends BlottiePlayer> = {
+  /**
+   * Lottie player loaded by Blottie
+   *
+   * @default 'undefined'
+   */
+  player?: T
+}
+
+type BlottieOptionsSvg = BlottieOptionsPart<'default'> | BlottieOptionsPart<'svg'> | BlottieOptionsPart<'light'> | BlottieOptionsPart<'worker'> & AnimationConfigWith<'svg'>
+
+type BlottieOptionsCanvas = BlottieOptionsPart<'canvas'> | BlottieOptionsPart<'canvas_worker'> | BlottieOptionsPart<'light_canvas'> & AnimationConfigWith<'canvas'>
+
+type BlottieOptionsHTML = BlottieOptionsPart<'html'> | BlottieOptionsPart<'light_html'> & AnimationConfigWith<'html'>
+
+export type BlottieOptions = BlottieOptionsSvg | BlottieOptionsCanvas | BlottieOptionsHTML
+
+export type BlottieOptionsComponent = BlottieOptions & {
+  /**
+   * Tag of the container
+   *
+   * @default 'div'
+   */
+  containerTag?: string
+  /**
+   * Callback before loadAnimation allowing to setLocationHref to fix Safari issue
+   *
+   * @see https://github.com/airbnb/lottie-web#issues
+   * @default 'undefined'
+   */
+  beforeInit?: (lottie: LottiePlayer) => Promise<void>
+}
